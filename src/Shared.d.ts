a64ce209d57e1e155bfef220fbd3cea5ccd6864f
nonamie <53796817+mathfox@users.noreply.github.com>
2024-07-12 02:36:48 +0300

    wip

--- src/Shared.d.ts
+++ src/Shared.d.ts
@@ -1,10 +1,15 @@
 interface PhantomSharedConstructor {
-    freeze<T>(this: void, array: Array<T>): ReadonlyArray<T>;
-    freeze<K, V>(this: void, map: Map<K, V>): ReadonlyMap<K, V>;
-    freeze<T>(this: void, set: Set<T>): ReadonlySet<T>;
-    freeze<T extends object>(this: void, o: T): Readonly<T>
+	freeze<T>(this: void, array: Array<T>): ReadonlyArray<T>;
+	freeze<K, V>(this: void, map: Map<K, V>): ReadonlyMap<K, V>;
+	freeze<T>(this: void, set: Set<T>): ReadonlySet<T>;
+	freeze<T extends object>(this: void, object: T): Readonly<T>;
+
+	values<T>(this: void, array: ReadonlyArray<T>): Array<T>;
+	values<T>(this: void, map: ReadonlyMap<defined, T>): Array<T>;
+	values(this: void, set: ReadonlySet<defined>): Array<true>;
+	values<T extends object, K extends keyof T>(dictionary: T): K[];
 }
 
-declare const PhantomShared: PhantomSharedConstructor
+declare const PhantomShared: PhantomSharedConstructor;
 
-export = PhantomShared
+export = PhantomShared;
--- src/test.ts
+++
@@ -1,9 +0,0 @@
-import { add } from "./Set"
-
-const value = new Set<number>()
-
-value.add(3)
-
-add(value, 3)
-
-const value2 = {} as Readonly<{ [k: string]: number }>
