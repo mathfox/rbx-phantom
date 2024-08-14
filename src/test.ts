47dd4f73af770511aabe3c392609a2a03b104f6b
nonamie <53796817+mathfox@users.noreply.github.com>
2024-08-14 20:48:42 +0300

    Delete test.ts

--- src/test.ts
+++
@@ -1,8 +0,0 @@
-import { add, deleteFromSet, difference } from "./Set";
-
-const set = new Set() as Set<"kek" | "lol">;
-
-const newSet = add(set, "kek2", "new3");
-const otherSet = deleteFromSet(newSet, "new3");
-
-const diff = difference(newSet, otherSet);
