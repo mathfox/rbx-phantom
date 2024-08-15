611a6e4a29524e1acc81d4b75941bd1794a7b216
nonamie <53796817+mathfox@users.noreply.github.com>
2024-08-15 15:18:01 +0300

    wip

--- src/Shared/index.d.ts
+++ src/Shared/index.d.ts
@@ -22,17 +22,48 @@ export type DeepReadonly<T> = T extends ReadonlyArray<infer R>
 	? DeepReadonlyArray<R>
 	: T extends Callback
 		? T
-		: T extends object
-			? DeepReadonlyObject<T>
-			: T;
+		: T extends ReadonlyMap<infer Key, infer Value>
+			? ReadonlyMap<Key, DeepReadonly<Value>>
+			: T extends object
+				? DeepReadonlyObject<T>
+				: T;
 
-export interface DeepReadonlyArray<T> extends ReadonlyArray<DeepReadonly<T>> {}
+//export interface DeepReadonlyArray<T> extends ReadonlyArray<DeepReadonly<T>> {}
+export type DeepReadonlyArray<T> = _<ReadonlyArray<DeepReadonly<T>>>;
 
-export type DeepReadonlyObject<T> = {
+export type DeepReadonlyObject<T> = _<{
 	readonly [P in keyof T]: DeepReadonly<T[P]>;
-};
+}>;
+
+export type DeepReadonlyMap<T> = T extends ReadonlyMap<infer Key, infer Value>
+	? ReadonlyMap<Key, DeepReadonly<Value>>
+	: never;
 
 interface PhantomSharedConstructor {
+	deepClone<T>(this: void, array: ReadonlyArray<T>): Array<T>;
+	deepClone<K, V>(this: void, map: ReadonlyMap<K, V>): Map<K, V>;
+	deepClone<T>(this: void, set: ReadonlySet<T>): Set<T>;
+	deepClone<T extends object>(object: T): T;
+
+	deepFreeze<T>(this: void, array: ReadonlyArray<T>): DeepReadonlyArray<T>;
+	deepFreeze<K, V>(
+		this: void,
+		map: ReadonlyMap<K, V>,
+	): ReadonlyMap<K, DeepReadonly<V>>;
+	deepFreeze<T>(this: void, set: ReadonlySet<T>): ReadonlySet<DeepReadonly<T>>;
+	deepFreeze<T extends object>(object: T): DeepReadonly<T>;
+
+	deepSafeFreeze<T>(this: void, array: ReadonlyArray<T>): DeepReadonlyArray<T>;
+	deepSafeFreeze<K, V>(
+		this: void,
+		map: ReadonlyMap<K, V>,
+	): ReadonlyMap<K, DeepReadonly<V>>;
+	deepSafeFreeze<T>(
+		this: void,
+		set: ReadonlySet<T>,
+	): ReadonlySet<DeepReadonly<T>>;
+	deepSafeFreeze<T extends object>(object: T): DeepReadonly<T>;
+
 	keys(this: void, array: ReadonlyArray<unknown>): Array<number>;
 	keys<K>(this: void, map: ReadonlyMap<K, unknown>): Array<K>;
 	keys<T>(this: void, set: ReadonlySet<T>): Array<T>;
