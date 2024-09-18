9bb5323d365b4b7cfe2ca41f1fe235816d2b8e7b
nonamie <53796817+mathfox@users.noreply.github.com>
2024-09-18 08:50:18 +0300

    fmt

--- src/Number/index.d.ts
+++ src/Number/index.d.ts
@@ -1,15 +1,15 @@
 import EPSILON from "./EPSILON";
-import isFinite from "./isFinite";
-import isInteger from "./isInteger";
-import isNaN from "./isNaN";
-import isSafeInteger from "./isSafeInteger";
 import MAX_SAFE_INTEGER from "./MAX_SAFE_INTEGER";
 import MAX_VALUE from "./MAX_VALUE";
 import MIN_SAFE_INTEGER from "./MIN_SAFE_INTEGER";
 import MIN_VALUE from "./MIN_VALUE";
-import NaN from "./NaN";
 import NEGATIVE_INFINITY from "./NEGATIVE_INFINITY";
+import NaN from "./NaN";
 import POSITIVE_INFINITY from "./POSITIVE_INFINITY";
+import isFinite from "./isFinite";
+import isInteger from "./isInteger";
+import isNaN from "./isNaN";
+import isSafeInteger from "./isSafeInteger";
 
 declare namespace PhantomNumber {
 	// constants
