eb87e3b6dbb42a3bc3d900db25febb5b77632f9b
nonamie <53796817+mathfox@users.noreply.github.com>
2024-09-20 13:02:02 +0300

    relax "isMap" criteria for number-like maps

--- src/Map/isMap.test.ts
+++ src/Map/isMap.test.ts
@@ -5,10 +5,22 @@ it("should return true if the value is a map", () => {
 	expect(isMap(new Map())).toBe(true);
 });
 
-it("should return try for empty tables", () => {
+it("should return true for empty tables", () => {
 	expect(isMap([])).toBe(true);
 
 	expect(isMap(new Set())).toBe(true);
 
 	expect(isMap({})).toBe(true);
+});
+
+it("should return true for Number-keyed maps", () => {
+	expect(
+		isMap(
+			new Map([
+				[0, "value"],
+				[1, "value"],
+				[2, "vaue"],
+			]),
+		),
+	).toBe(true);
 });
