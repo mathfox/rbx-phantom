424db801ad134450c3f9296e45447d7bca6be698
nonamie <53796817+mathfox@users.noreply.github.com>
2024-09-26 01:58:12 +0300

    "deepClone" now deeply clone keys

--- src/Shared/deepClone.test.ts
+++ src/Shared/deepClone.test.ts
@@ -1,5 +1,6 @@
 import { expect, it } from "@rbxts/jest-globals";
 import deepClone from "./deepClone";
+import { deepCompare } from "./deepCompare";
 
 it("should return a copy of the given array", () => {
 	const array = [1, 2, 3];
@@ -27,4 +28,25 @@ it("should maintain the metatable reference when cloning", () => {
 
 	expect(getmetatable(array)).toBe(metatable);
 	expect(getmetatable(clone)).toBe(metatable);
+});
+
+it("should clone the keys", () => {
+	const key = {};
+	const value = new Map([[key, 1]]);
+	expect(deepClone(value)).never.toEqual(value);
+});
+
+it("should clone nested keys and values", () => {
+	const obj = new Map([
+		[
+			new Map([
+				[0, "foo"],
+				[1, "bar"],
+			]),
+			new Map([[3, "baz"]]),
+		],
+	]);
+	const clonedObj = deepClone(obj);
+
+	expect(deepCompare(obj, clonedObj)).toBe(true);
 });
